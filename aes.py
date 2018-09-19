ef2ef89e6d913717c881d229156cb4c304c6efad
longhorn396 <devin@drawhorn.com>
2018-09-19 01:38:57 -0500

    Encryption works; added second test files

--- aes.py
+++ aes.py
@@ -1,30 +1,56 @@
-#! /usr/local/bin/python3
+#! /usr/bin/python3
 
-import sys, getopt
-import common_arrays
-import encrypt
+import sys, getopt, array
+import common_arrays, encrypt
 
 class AESComponent:
 
+    @staticmethod
     def to_col_order_matrix(arr):
         matrix = [[0 for _ in range(0, 4)] for _ in range(0, 4)]
-        index = 0
-        for byte in arr:
+        for index, byte, in enumerate(arr):
             matrix[index % 4][index // 4] = byte
-            index += 1
         return matrix
 
+    @staticmethod
     def collapse_matrix(matrix, out_array):
         for j in range(0, 4):
             for i in range(0, 4):
                 out_array.append(matrix[i][j])
-
-    def add_round_key(matrix, key, index):
+        return out_array
+    
+    @staticmethod
+    def expand_key(key, nk, nr):
+        w = [word for word in key]
+        for i in range(nk, 4 * (nr + 1)):
+            temp = w[4*(i-1):4*i]
+            if i % nk == 0:
+                temp = [common_arrays.sbox[byte] for byte in temp[1:] + temp[:1]]
+                temp[0] = temp[0] ^ common_arrays.Rcon[i // nk]
+            elif nk == 8 and i % nk == 4:
+                temp = [common_arrays.sbox[byte] for byte in temp]
+            for j in range(0, 4):
+                w.append(w[4 * (i - nk) + j] ^ temp[j])
+        return w
+    
+    @staticmethod
+    def sub_bytes(state, box):
+        return [[box[byte] for byte in word] for word in state]
+    
+    @staticmethod
+    def shift_rows(state, l):
+        new_state = [word[:] for word in state]
+        for i in range(1, len(state)):
+            for j in range(0, 4):
+                new_state[i][j] = state[i][(l(i, j)) % 4]
+        return new_state
+    
+    @staticmethod
+    def add_round_key(state, key):
         for j in range(0, 4):
             for i in range(0, 4):
-                matrix[i][j] ^= key[index]
-                index += 1
-        return matrix
+                state[i][j] ^= key[i][j]
+        return state
 
 def print_help(exit_code):
     # TODO: better usage message
@@ -38,32 +64,33 @@ def main(argv):
     mode = None
 
     try:
-      opts, args = getopt.getopt(argv,"hedi:o:k:",["help","encrypt","decrypt","ifile=","ofile=","kfile="])
+      opts, _ = getopt.getopt(argv,"hedi:o:k:",["help","encrypt","decrypt","ifile=","ofile=","kfile="])
     except getopt.GetoptError:
         print_help(2)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print_help(0)
         elif opt in ("-i", "--ifile"):
-            input_file = bytearray(open(arg, "rb").read())
+            input_file = open(arg, "rb").read()
         elif opt in ("-o", "--ofile"):
             output_file = open(arg, "wb")
         elif opt in ("-k", "--keyfile"):
-            key_file = bytearray(open(arg, "rb").read())
+            key_file = open(arg, "rb").read()
         elif opt in ("-e", "--encrypt"):
             mode = 0
         elif opt in ("-d", "--decrypt"):
             mode = 1
+        
+    component = None
+    if mode == 0:
+        component = encrypt.AESEncryptor()
+    if mode == 1:
+        component = 0
     
-    i = bytearray(open(input_file, "rb").read())
-    k = bytearray(open(input_file, "rb").read())
-    o = open(output_file, "wb")
-
-    print("Read files, input is {0} in length, key is {1} in length".format(len(i), len(k)))
-    print("input:")
-    print(i)
-    print("key")
-    print(k)
+    output = component.aes(input_file, AESComponent.expand_key(key_file, 4, 10), 10)
+    output = array.array('B', output)
+    output_file.write(output)
+    output_file.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ encrypt.py
@@ -0,0 +1,50 @@
+from aes import AESComponent
+from common_arrays import sbox, gfp2, gfp3
+
+def myprint(state):
+    out = ""
+    for i in range(0, 4):
+        for j in range(0, 4):
+            out += str(hex(state[i][j])) + " "
+    print(out)
+
+class AESEncryptor(AESComponent):
+
+    def aes(self, block, key, nr):
+        # pad = 16 - (len(block) % 16)
+        # block.extend([0] * pad)
+        # block[-1] = pad
+        output = []
+        for i in range(0, len(block), 16):
+            output = AESComponent.collapse_matrix(self.cipher(block[i:i+16], key, nr), output)
+        return output
+    
+    def cipher(self, block, key, nr):
+        state = AESComponent.to_col_order_matrix(block)
+        state = AESComponent.add_round_key(state, AESComponent.to_col_order_matrix(key[:16]))
+        for i in range(1, nr):
+            state = self.sub_bytes(state)
+            state = self.shift_rows(state)
+            state = self.mix_columns(state)
+            state = AESComponent.add_round_key(state, AESComponent.to_col_order_matrix(key[16*i:16*(i+1)]))
+        state = self.sub_bytes(state)
+        state = self.shift_rows(state)
+        state = AESComponent.add_round_key(state, AESComponent.to_col_order_matrix(key[-16:]))
+        return state
+
+    def sub_bytes(self, state):
+        return AESComponent.sub_bytes(state, sbox)
+    
+    def shift_rows(self, state):
+        return AESComponent.shift_rows(state, lambda x, y : x + y)
+    
+    def mix_columns(self, state):
+        temp = [0 for _ in range(0, 4)]
+        for i in range(0, 4):
+            temp[0] = gfp2[state[0][i]] ^ gfp3[state[1][i]] ^ state[2][i] ^ state[3][i]
+            temp[1] = state[0][i] ^ gfp2[state[1][i]] ^ gfp3[state[2][i]] ^ state[3][i]
+            temp[2] = state[0][i] ^ state[1][i] ^ gfp2[state[2][i]] ^ gfp3[state[3][i]]
+            temp[3] = gfp3[state[0][i]] ^ state[1][i] ^ state[2][i] ^ gfp2[state[3][i]]
+            for j in range(0, 4):
+                state[j][i] = temp[j]
+        return state
