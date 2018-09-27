772470a0d457c40352bb0365dc4822d7e7dd9f55
Devin Drawhorn <devin@drawhorn.com>
2018-09-27 14:35:34 -0500

    README documentation of the rest of the python files

--- encrypt.py
+++ encrypt.py
@@ -9,7 +9,7 @@ class AESEncryptor(AESComponent):
         if verbose:
             self.verbose = True
         pad = 16 - (len(block) % 16)
-        block.extend([0] * pad)
+        block.append([0] * pad)
         block[-1] = pad
         output = []
         for i in range(0, len(block), 16):
